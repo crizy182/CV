9f8549ee318164555b779b240c54fb45b35a0bc9
crizy182 <cristhiam.tovar@mail.escuelaing.edu.co>
2019-05-23 21:00:22 -0500

    Listo para presentar faltan, detalles como el icono de whatsapp

--- static/js/eventlistener.js
+++ static/js/eventlistener.js
@@ -1,49 +1,29 @@
-$(document).ready(function() {
-    // to start the labels is hide
-    $("#name").on("keyup" , function ()
-    {
-   var value = $(this).val().length;
-   if (value > 0) {
-     $("#name-label").show()
-   }
-  else {
-    $("#name-label").hide()
+const ipad = window.matchMedia('screen and (max-width: 769px)');
+const menu = document.querySelector('.menu');
+const burgerButton = document.querySelector('#burger-menu');
+
+console.log(burgerButton);
+function showHide() {
+  if (menu.classList.contains('is-active')) {
+    menu.classList.remove('is-active');
+  } else {
+    menu.classList.add('is-active');
   }
-   })
+}
 
-   $("#email").on("keyup" , function ()
-   {
-  var value = $(this).val().length;
-  if (value > 0) {
-    $("#email-label").show()
+if (ipad.matches)
+  burgerButton.addEventListener('click', hideShow);
+ipad.addListener((event) => {
+  if (event.matches)
+    burgerButton.addEventListener('click', hideShow);
+  else
+    burgerButton.removeEventListener('click', hideShow);
+});
+
+function hideShow() {
+  if (menu.classList.contains('is-active')) {
+    menu.classList.remove('is-active');
+  } else {
+    menu.classList.add('is-active');
   }
- else {
-   $("#email-label").hide()
- }
-  })
-
-  $("#phone").on("keyup" , function ()
-  {
- var value = $(this).val().length;
- if (value > 0) {
-   $("#phone-label").show()
- }
-else {
-  $("#phone-label").hide()
 }
- })
-
- $("#message").on("keyup" , function ()
- {
-var value = $(this).val().length;
-if (value > 0) {
-  $("#message-label").show()
-}
-else {
- $("#message-label").hide()
-}
-})
-
-console.log(scrollY);
-    
-})
